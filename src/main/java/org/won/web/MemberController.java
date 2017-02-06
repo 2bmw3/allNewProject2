4ea584580625bc430ffd1b7733e68da62f46e51d
2bmw3 <2bmw3@DESKTOP-HQE6N0F>
2017-02-06 17:54:59 +0900

    crossorigin edit

--- src/main/java/org/won/web/MemberController.java
+++ src/main/java/org/won/web/MemberController.java
@@ -13,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -64,6 +65,7 @@ public class MemberController {
 
 	/* 멤버 전체 리스트 */
 	@GetMapping("/list")
+	@CrossOrigin(origins="*")
 	public @ResponseBody List<ProductsVO> list(Model model) throws Exception {
 //		model.addAttribute("list", pservice.mTotalList());
 		return pservice.mTotalList();
